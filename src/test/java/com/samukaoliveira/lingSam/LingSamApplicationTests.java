2eaf2c955211730259b9a4164d0cdc3ecc001de0
Samuel <mucabatera@gmail.com>
2026-07-14 20:39:30 -0300

    alterando test.properties 2

--- src/test/java/com/samukaoliveira/lingSam/LingSamApplicationTests.java
+++ src/test/java/com/samukaoliveira/lingSam/LingSamApplicationTests.java
@@ -2,8 +2,10 @@ package com.samukaoliveira.lingSam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LingSamApplicationTests {
 
 	@Test
